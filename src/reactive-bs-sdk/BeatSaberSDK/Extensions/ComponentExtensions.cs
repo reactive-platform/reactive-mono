c3ed79923f46bce1ea4c95946674990977f12f8b
Hermanest <mutcmk@gmail.com>
2025-03-06 18:32:39 +0300

    Added button extensions.

--- src/reactive-bs-sdk/BeatSaberSDK/Extensions/ComponentExtensions.cs
+++ src/reactive-bs-sdk/BeatSaberSDK/Extensions/ComponentExtensions.cs
@@ -1,6 +1,8 @@
 using HMUI;
 using JetBrains.Annotations;
 using Reactive.Components;
+using Reactive.Yoga;
+using TMPro;
 using UnityEngine;
 using UImage = UnityEngine.UI.Image;
 
@@ -8,6 +10,106 @@ namespace Reactive.BeatSaber.Components;
 
 [PublicAPI]
 public static class ComponentExtensions {
+    #region Button
+
+    public static T WithLabel<T>(
+        this T button,
+        string text,
+        float fontSize = 4f,
+        bool richText = true,
+        TextOverflowModes overflow = TextOverflowModes.Overflow
+    ) where T : ButtonBase, IChildrenProvider {
+        return WithLabel(
+            button,
+            out _,
+            text,
+            fontSize,
+            richText,
+            overflow
+        );
+    }
+
+    public static T WithLabel<T>(
+        this T button,
+        out Label label,
+        string text,
+        float fontSize = 4f,
+        bool richText = true,
+        TextOverflowModes overflow = TextOverflowModes.Overflow
+    ) where T : ButtonBase, IChildrenProvider {
+        button.AsFlexGroup(alignItems: Align.Center);
+        button.Children.Add(
+            new Label {
+                Text = text,
+                FontSize = fontSize,
+                RichText = richText,
+                Overflow = overflow
+            }.With(
+                x => {
+                    if (button is not ISkewedComponent skewed) {
+                        return;
+                    }
+                    ((ISkewedComponent)x).Skew = skewed.Skew;
+                }
+            ).AsFlexItem(
+                size: "auto",
+                margin: new() { left = 2f, right = 2f }
+            ).Export(out label)
+        );
+        return button;
+    }
+
+    public static T WithImage<T>(
+        this T button,
+        Sprite? sprite,
+        Color? color = null,
+        float? pixelsPerUnit = null,
+        bool preserveAspect = true,
+        UImage.Type type = UImage.Type.Simple,
+        Optional<Material>? material = default
+    ) where T : ButtonBase, IChildrenProvider {
+        return WithImage(
+            button,
+            out _,
+            sprite,
+            color,
+            pixelsPerUnit,
+            preserveAspect,
+            type,
+            material
+        );
+    }
+
+    public static T WithImage<T>(
+        this T button,
+        out Image image,
+        Sprite? sprite,
+        Color? color = null,
+        float? pixelsPerUnit = null,
+        bool preserveAspect = true,
+        UImage.Type type = UImage.Type.Simple,
+        Optional<Material>? material = default
+    ) where T : ButtonBase, IChildrenProvider {
+        button.Children.Add(
+            new Image {
+                Sprite = sprite,
+                Material = material.GetValueOrDefault(GameResources.UINoGlowMaterial),
+                Color = color ?? Color.white,
+                PixelsPerUnit = pixelsPerUnit ?? 0f,
+                ImageType = pixelsPerUnit == null ? type : UImage.Type.Sliced,
+                PreserveAspect = preserveAspect
+            }.With(
+                x => {
+                    if (button is not ISkewedComponent skewed) return;
+                    x.Skew = skewed.Skew;
+                }
+            ).AsFlexItem(grow: 1f).Export(out image)
+        );
+        return button;
+    }
+
+    #endregion
+
     #region Image
 
     [Pure]
