1aed3071551649b2b2326ed7c97b834c1e2c1eb7
Hermanest <mutcmk@gmail.com>
2025-06-02 12:37:13 +0300

    Added a table overload after reactive-sdk update.

--- src/reactive-bs-sdk/BeatSaberSDK/Components/Table/Table.cs
+++ src/reactive-bs-sdk/BeatSaberSDK/Components/Table/Table.cs
@@ -4,6 +4,13 @@ using Reactive.Components;
 namespace Reactive.BeatSaber.Components;
 
 [PublicAPI]
+public class Table<TItem> : Reactive.Components.Basic.Table<TItem> {
+    protected override Reactive.Components.Basic.ScrollArea ConstructScrollArea() {
+        return new ScrollArea();
+    }
+}
+
+[PublicAPI]
 public class Table<TItem, TCell> : Reactive.Components.Basic.Table<TItem, TCell> where TCell : ITableCell<TItem>, IReactiveComponent, new() {
     protected override Reactive.Components.Basic.ScrollArea ConstructScrollArea() {
         return new ScrollArea();
