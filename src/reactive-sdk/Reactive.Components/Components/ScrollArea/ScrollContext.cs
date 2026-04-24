d7dcdd5b86ffa651d72618024bea6ae10bcb969d
hermanest <mutcmk@gmail.com>
2026-04-24 16:20:07 +0300

    Added more properties to ScrollContext.

--- src/reactive-sdk/Reactive.Components/Components/ScrollArea/ScrollContext.cs
+++ src/reactive-sdk/Reactive.Components/Components/ScrollArea/ScrollContext.cs
@@ -1,31 +1,194 @@
+using System;
 using JetBrains.Annotations;
+using UnityEngine;
 
 namespace Reactive.Components;
 
-[PublicAPI]
-public readonly record struct ScrollContext(float ScrollPos, bool Immediately) {
-    /// <summary>
-    /// Scrolls to the specified point.
-    /// </summary>
-    /// <param name="pos">A pos to scroll to.</param>
-    /// <param name="immediately">Whether to apply the scroll immediately.</param>
-    /// <returns>An updated context.</returns>
-    public ScrollContext ScrollTo(float pos, bool immediately = false) {
-        return new() {
-            ScrollPos = pos,
-            Immediately = immediately
-        };
-    }
+public enum ScrollUpdateType {
+    None,
+    /// User intent to scroll (e.g. ScrollPos, ScrollItem).
+    Intent,       
+    /// Controller measurements (e.g. ContentSize, ViewSize).
+    Measurements,  
+    /// Controller scroll (ActualScrollPos).
+    InternalScroll
 }
 
 [PublicAPI]
-public static class ScrollContextExtension {
+public class ScrollContext : IState<ScrollContext> {
     /// <summary>
-    /// Updates the state with a new scroll context.
+    /// Determines the current scroll pos. Set by the user.
     /// </summary>
-    /// <param name="pos">A pos to scroll to.</param>
-    /// <param name="immediately">Whether to apply the scroll immediately.</param>
-    public static void ScrollTo(this IMutableState<ScrollContext> state, float pos, bool immediately = false) {
-        state.Value = state.Value.ScrollTo(pos, immediately);
+    public float ScrollPos { get; private set; }
+
+    /// <summary>
+    /// Determines the item that the controller should scroll to.
+    /// Has higher priority than <see cref="ScrollPos"/>, so if both are specified the first one is ignored.
+    /// Defaults to <see cref="ScrollPos"/> if the specified object isn't presented in the view.
+    /// </summary>
+    public object? ScrollItem { get; private set; }
+
+    /// Whether the scroll animation should finish immediately. Set by the user.
+    public bool Immediately { get; private set; }
+
+    /// <summary>
+    /// The actual scroll pos of the scroll controller at the moment.
+    /// Set by a component controlling the view.
+    /// </summary>
+    public float ActualScrollPos { get; private set; }
+
+    /// <summary>
+    /// The size of a single line inside the scroll content.
+    /// Set by a component controlling the view.
+    /// </summary>
+    public float LineSize { get; private set; }
+    
+    /// <summary>
+    /// The size of the scroll content. Set by a component controlling the view.
+    /// </summary>
+    public float ContentSize { get; private set; }
+
+    /// <summary>
+    /// The size of the viewport. Set by a component controlling the view.
+    /// </summary>
+    public float ViewSize { get; private set; }
+    
+    /// <summary>
+    /// Determines a kind of the last update. Returns None if called outside the ValueChanged cycle.
+    /// </summary>
+    public ScrollUpdateType UpdateType { get; private set; }
+
+    /// <summary>
+    /// Helper to get or set scroll position in a 0..1 range.
+    /// </summary>
+    public float NormalizedScrollPos {
+        get {
+            var range = ContentSize - ViewSize;
+            return range > 0 ? ActualScrollPos / range : 0f;
+        }
     }
+
+    public float MaxScrollPos {
+        get => ContentSize - ViewSize;
+    }
+    
+    /// <summary>
+    /// Scrolls to the specified point.
+    /// </summary>
+    /// <param name="pos">The position to scroll to.</param>
+    /// <param name="immediately">Whether to apply the scroll immediately.</param>
+    /// <returns>An updated context.</returns>
+    public void ScrollTo(float pos, bool immediately = false) {
+        var newPos = Mathf.Clamp(pos, 0f, MaxScrollPos);
+        
+        if (Mathf.Approximately(newPos, ScrollPos)) {
+            return;
+        }
+
+        ScrollPos = newPos;
+        ScrollItem = null;
+        Immediately = immediately;
+        
+        NotifyValueChanged(ScrollUpdateType.Intent);
+    }
+
+    /// <summary>
+    /// Scrolls to the specified item. Defaults 
+    /// </summary>
+    /// <param name="pos">The position to scroll to.</param>
+    /// <param name="immediately">Whether to apply the scroll immediately.</param>
+    /// <returns>An updated context.</returns>
+    public void ScrollTo(object item, bool immediately = false) {
+        ScrollItem = item;
+        Immediately = immediately;
+        NotifyValueChanged(ScrollUpdateType.Intent);
+    }
+    
+    public void ScrollRelative(float offset, bool immediately = false) {
+        ScrollTo(ScrollPos + offset, immediately);
+    }
+
+    /// <summary>
+    /// Scrolls down for a size of the viewport.
+    /// </summary>
+    /// <param name="immediately">Whether to apply the scroll immediately.</param>
+    public void PageDown(bool immediately = false) {
+        ScrollRelative(ViewSize, immediately);
+    }
+
+    /// <summary>
+    /// Scrolls up for a size of the viewport.
+    /// </summary>
+    /// <param name="immediately">Whether to apply the scroll immediately.</param>
+    public void PageUp(bool immediately = false) {
+        ScrollRelative(ViewSize * -1, immediately);
+    }
+
+    public void LineDown(bool immediately = false) {
+        ScrollRelative(LineSize, immediately);
+    }
+
+    public void LineUp(bool immediately = false) {
+        ScrollRelative(LineSize, immediately);
+    }
+
+    /// <summary>
+    /// Sets internal measurements of the scroll controller. Shouldn't be called manually.
+    /// </summary>
+    /// <param name="contentSize">The size of the content.</param>
+    /// <param name="viewSize">The size of the viewport.</param>
+    public void SetInternalMeasurements(float contentSize, float viewSize) {
+        ContentSize = contentSize;
+        ViewSize = viewSize;
+        NotifyValueChanged(ScrollUpdateType.Measurements);
+    }
+
+    /// <summary>
+    /// Sets internal scroll of the scroll controller at the moment. Shouldn't be called manually.
+    /// </summary>
+    public void SetInternalScrollPos(float pos) {
+        if (Mathf.Approximately(ActualScrollPos, pos)) {
+            return;
+        }
+        
+        ActualScrollPos = pos;
+        NotifyValueChanged(ScrollUpdateType.InternalScroll);
+    }
+
+    #region State Impl
+
+    ScrollContext IState<ScrollContext>.Value => this;
+
+    public event Action<ScrollContext>? ValueChangedEvent;
+
+    private void NotifyValueChanged(ScrollUpdateType type) {
+        UpdateType = type;
+        ValueChangedEvent?.Invoke(this);
+        
+        UpdateType = ScrollUpdateType.None;
+    }
+
+    public override bool Equals(object? obj) {
+        // ReSharper disable CompareOfFloatsByEqualityOperator
+        return obj is ScrollContext context &&
+            context.Immediately.Equals(Immediately) &&
+            context.ScrollPos.Equals(ScrollPos) &&
+            context.ActualScrollPos.Equals(ActualScrollPos) &&
+            context.ContentSize.Equals(ContentSize) &&
+            context.ViewSize.Equals(ViewSize);
+    }
+
+    public override int GetHashCode() {
+        // ReSharper disable NonReadonlyMemberInGetHashCode
+        unchecked {
+            var hashCode = ScrollPos.GetHashCode();
+            hashCode = (hashCode * 397) ^ Immediately.GetHashCode();
+            hashCode = (hashCode * 397) ^ ActualScrollPos.GetHashCode();
+            hashCode = (hashCode * 397) ^ ContentSize.GetHashCode();
+            hashCode = (hashCode * 397) ^ ViewSize.GetHashCode();
+            return hashCode;
+        }
+    }
+
+    #endregion
 }
